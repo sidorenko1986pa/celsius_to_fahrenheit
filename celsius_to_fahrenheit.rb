12796e1ef45d023028a4a12af930dcea654250a8
Pavel Sidorenko <pavel@cifronomika.ru>
2020-03-12 21:48:21 +0300

    two

--- celsius_to_fahrenheit.rb
+++ celsius_to_fahrenheit.rb
@@ -1,19 +1,20 @@
-class ConverterCelsiusToFahrenheit
-  def initialize(celsius)
-    @celsius = celsius[/^([+-]?\d+(.[\d{0,1}])?)$/]
-    freeze
-  end
+def make(celsius)
+  celsius = celsius[/^([+-]?\d+(.[\d{0,2}+])?)$/]
+  return 'Неверно указано значение' if celsius.nil?
 
-  def make
-    return 'Неверно указано значение' if @celsius.nil?
+  "#{converter(celsius)} °F"
+end
 
-    "#{converter} °F"
-  end
+def converter(celsius)
+  result = (celsius.to_f * 1.8 + 32).round(1)
+  return result.to_i if integer?(result)
 
-  def converter
-    (@celsius.to_f * 1.8 + 32).round(1)
-  end
+  result
+end
+
+def integer?(number)
+  number.to_s[/^([+-]?\d+.0)$/]
 end
 
 puts "Введите градусы по Цельсию(одно значение после запятой):"
-puts ConverterCelsiusToFahrenheit.new(gets.chomp).make
+puts make(gets.chomp)
